95a0ac7a4b092cb24481194faa0791624c9eef6f
rainy.city <d.barrowcliff@outlook.com>
2022-04-07 18:57:21 +0100

    izy iz

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">GINA w/ Shannon from Admin & Team GB</p>
+                  <p className="audio-player-text">Izzy Iz w/ Will Druid</p>
                 </div>
               </ul>
             </div>
