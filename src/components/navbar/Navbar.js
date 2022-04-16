a60dcb906f99227d132b8605512c938831e36988
rainy.city <d.barrowcliff@outlook.com>
2022-04-16 19:59:46 +0100

    dfg

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Lip Service</p>
+                  <p className="audio-player-text">Lady Marmaduke w/ Dutty Boy Reflex</p>
                 </div>
               </ul>
             </div>
