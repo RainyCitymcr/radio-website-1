00ba7c53f24791744a5bd9d4c2903ba5f91632a1
rainy.city <d.barrowcliff@outlook.com>
2022-04-16 18:04:31 +0100

    charm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">CHARMS</p>
+                  <p className="audio-player-text">CHARM</p>
                 </div>
               </ul>
             </div>
