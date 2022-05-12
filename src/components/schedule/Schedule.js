d7f6235731d933c7e8a7257be69f14bbc44a83c5
rainy.city <d.barrowcliff@outlook.com>
2022-05-12 18:25:21 +0100

    scheudkle

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,49 +11,61 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 21st April"
-                />
-                <ScheduleItem
-                    showTime="18:00 - 20:00"
-                    showName="MIRE"
-                />
-                <ScheduleItem
-                    showTime="20:00 - 21:00"
-                    showName="Relax and Bake FM"  
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Friday 22nd April"
-                    />
-                    <ScheduleItem
-                        showTime="19:00 - 20:00"
-                        showName="DJ Project Manager"
-                    />
-                    <ScheduleItem
-                        showTime="20:00 - 21:00"
-                        showName="Wobbegong"
-                    />
-                    <ScheduleItem
-                        showTime="21:00 - 22:00"
-                        showName= "Guessef"
-                />
-            </table>
-            <table className="schedule-table">
-                <ScheduleDate 
-                    showDate="Saturday 22nd April"
+                    showDate="Thursday 12th May"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Sav"
+                    showName="Line:156"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Charlie B"
+                    showName="Earl Grey"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Halfs"  
+                    showName="Ship Sket w/ Tee"    
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="bicho raro"    
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Friday 13th May"
+                    />
+                    <ScheduleItem
+                        showTime="19:00 - 20:00"
+                        showName="High Voltage"
+                    />
+                    <ScheduleItem
+                        showTime="20:00 - 21:00"
+                        showName="Alfaz"
+                    />
+                    <ScheduleItem
+                        showTime="21:00 - 22:00"
+                        showName= "Belladonna Bitch Fist"
+                />
+            </table>
+            <table className="schedule-table">
+                <ScheduleDate 
+                    showDate="Saturday 14th May"
+                />
+                <ScheduleItem
+                    showTime="18:00 - 19:00"
+                    showName="Felicia Buena"
+                />
+                <ScheduleItem
+                    showTime="19:00 - 20:00"
+                    showName="Liil Bits"
+                />
+                <ScheduleItem
+                    showTime="20:00 - 21:00"
+                    showName="Lady Marmaduke & Lintd"  
+                />
+                <ScheduleItem
+                    showTime="21:00 - 22:00"
+                    showName="Miss Bad B"  
                 />
             </table>
         </div>
