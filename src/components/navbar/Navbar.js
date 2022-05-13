ef6e64ce4370d4fb86cf04bbf065e6b347f3fc4f
rainy.city <d.barrowcliff@outlook.com>
2022-05-13 20:40:25 +0100

    mia hats

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Alfaz</p>
+                  <p className="audio-player-text">Belladonna Bitch Fist</p>
                 </div>
               </ul>
             </div>
