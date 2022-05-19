e525a1707d3844bda007b9920c38f1a66ecc6da2
rainy.city <d.barrowcliff@outlook.com>
2022-05-19 19:53:35 +0100

    Relax and bake fm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-container">MIRE</p>
+                  <p className="audio-player-container">Relax and Bake FM</p>
                 </div>
               </ul>
             </div>
