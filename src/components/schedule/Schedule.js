e802669d9c14efc334d78382a4ecaae8a0027aad
rainy.city <d.barrowcliff@outlook.com>
2022-05-20 18:39:25 +0100

    on leah

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -11,40 +11,36 @@ const Schedule = () => {
             <h1 className="schedule-title">SCHEDULE</h1>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 12th May"
+                    showDate="Thursday 19th May"
                 />
                 <ScheduleItem
                     showTime="18:00 - 19:00"
-                    showName="Line:156"
+                    showName="MIRE"
                 />
                 <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Earl Grey"
+                    showName="Relax and Bake FM"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Ship Sket w/ Tee"    
-                />
-                <ScheduleItem
-                    showTime="21:00 - 22:00"
-                    showName="bicho raro"    
+                    showName="Trash Bandicoot"    
                 />
             </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Friday 13th May"
+                    showDate="Friday 20th May"
                     />
                     <ScheduleItem
                         showTime="19:00 - 20:00"
-                        showName="High Voltage"
+                        showName="DJ Project Manager"
                     />
                     <ScheduleItem
                         showTime="20:00 - 21:00"
-                        showName="Alfaz"
+                        showName="Combi Completa"
                     />
                     <ScheduleItem
                         showTime="21:00 - 22:00"
-                        showName= "Belladonna Bitch Fist"
+                        showName= "Dan Traynor"
                 />
             </table>
             <table className="schedule-table">
@@ -52,20 +48,20 @@ const Schedule = () => {
                     showDate="Saturday 14th May"
                 />
                 <ScheduleItem
-                    showTime="18:00 - 19:00"
-                    showName="Felicia Buena"
-                />
-                <ScheduleItem
                     showTime="19:00 - 20:00"
-                    showName="Liil Bits"
+                    showName="Charlie B presents: Tram Man"
                 />
                 <ScheduleItem
                     showTime="20:00 - 21:00"
-                    showName="Lady Marmaduke & Lintd"  
+                    showName="Harry Amstel b2b DJ Simcard"
                 />
                 <ScheduleItem
                     showTime="21:00 - 22:00"
-                    showName="Miss Bad B"  
+                    showName="Flushed Aay w/ Dom Thompson"  
+                // />
+                // <ScheduleItem
+                //     showTime="21:00 - 22:00"
+                //     showName="Miss Bad B"  
                 />
             </table>
         </div>
