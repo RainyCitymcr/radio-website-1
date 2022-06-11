bc6a5a7b51b1c657d935ccd7dee7db0a3d80da7d
rainy.city <d.barrowcliff@outlook.com>
2022-06-11 18:51:05 +0100

    mike

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text"></p>
+                  <p className="audio-player-text">Half Edge w/ Mike Smaczylo</p>
                 </div>
               </ul>
             </div>
