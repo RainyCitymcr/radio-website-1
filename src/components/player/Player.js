270671364f807daf61b579ae3e5f3110097755a2
Rainycitymcr <d.barrowcliff@outlook.com>
2022-06-13 17:31:47 +0100

    player

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -22,7 +22,7 @@ const Player = () => {
 
     return (
         <div className="player-wrapper">
-            <audio ref={audioPlayer} src="http://radio.rainycityradio.live:8000/radio.mp3" preload="metadata"></audio>
+            <audio ref={audioPlayer} src="https://l.facebook.com/l.php?u=https%3A%2F%2Frainycityradio.out.airtime.pro%2Frainycityradio_a%3Ffbclid%3DIwAR2wo7KmyaoW0dQ_xMXG0_7k5fQI4OoaeD4T4xpqZjElmwWgcRNBy0gPFFg&h=AT0EbrDtsY3y_bpZKT1H-YfQr6uuh9ewu0Y97fejdiUGMgiooKuvwLQRo-VlqVge7sGaGh-lomnw9R77IQxhvFzTbQgktuxs3Lp4ablLbyn3fc4PtW-gmZfgaenRPR8kOy_DO9qQZpzwQzuKpYI" preload="metadata"></audio>
             {/* Remove "disabled" from the button tag below. */}
             <button className="play-btn" onClick={togglePlayPause}>{ isPlaying ? <FaStop /> : <FaPlay /> }</button>
         </div>
