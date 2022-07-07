6fcb9c3a431fed0d93e8f81f775b720db9d64bc9
rainy.city <d.barrowcliff@outlook.com>
2022-07-07 19:27:45 +0100

    herbal tea

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">izzy iz</p>
+                  <p className="audio-player-text">Herbal Tea</p>
                 </div>
               </ul>
             </div>
