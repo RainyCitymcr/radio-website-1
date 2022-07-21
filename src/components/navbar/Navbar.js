b6df45da7210914d6a421bfbe401c1151273da21
rainy.city <d.barrowcliff@outlook.com>
2022-07-21 21:55:19 +0100

    poorj

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Into The Mists: The Selkie</p>
+                  <p className="audio-player-text">Poor J'Darr w/ Cross, Hogg and 4Tgang</p>
                 </div>
               </ul>
             </div>
