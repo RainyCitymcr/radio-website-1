373b7dbb6c6d2cf3ac356054bdaa4a46c59b8b9b
rainy.city <d.barrowcliff@outlook.com>
2022-09-08 21:00:38 +0100

    carmen

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">After School Club w/ Teaching Assistant </p>
+                  <p className="audio-player-text">interview with Lucy Rose Cunningham (Carmen Walker-vazquez & Dany Bowen) </p>
                 </div>
               </ul>
             </div>
