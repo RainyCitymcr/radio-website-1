ff605805e72d5738ee39707b764502a247d1ebe9
rainy.city <d.barrowcliff@outlook.com>
2022-09-15 20:51:39 +0100

    sfm

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">relax and bake fm w/ monique</p>
+                  <p className="audio-player-text">sfm</p>
                 </div>
               </ul>
             </div>
