3494fc6fa200cda2566061b266874e3ba37b6415
rainy.city <d.barrowcliff@outlook.com>
2022-10-08 17:01:33 +0100

    f

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">great danes takeove w/ thoden</p>
+                  <p className="audio-player-text">great danes takeover w/ thoden</p>
                 </div>
               </ul>
             </div>
