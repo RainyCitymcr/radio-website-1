15f47c8769094f16ae680641590223a1cfdedca7
Rainycitymcr <d.barrowcliff@outlook.com>
2022-11-04 12:01:29 +0000

    on

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -113,11 +113,11 @@ const Navbar = () => {
               <ul>
                 <li><div className="circle red"></div></li>
                 {/* Write "ON" instead of "OFF" (or vice versa) */}
-                <li>off air</li>
+                <li>on air</li>
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text"></p>
+                  <p className="audio-player-text">EJ & rosa live @ The Loft</p>
                 </div>
               </ul>
             </div>
