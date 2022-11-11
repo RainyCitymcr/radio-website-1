105c386671273d4baa397c1ba1af9fc2fc695493
rainy.city <d.barrowcliff@outlook.com>
2022-11-11 19:22:36 +0000

    mc

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">deaf loop</p>
+                  <p className="audio-player-text">mc love</p>
                 </div>
               </ul>
             </div>
