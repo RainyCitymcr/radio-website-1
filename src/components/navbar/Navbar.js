0241616102fabad28ee7bc666596c4533aae4132
rainy.city <d.barrowcliff@outlook.com>
2022-11-12 18:31:27 +0000

    slf

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">mbb b2b ayse</p>
+                  <p className="audio-player-text">So la flair takeover: mbb b2b ayse</p>
                 </div>
               </ul>
             </div>
