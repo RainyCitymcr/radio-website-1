059660937fa5e04f9282b86c462633a0d1219448
Rainycitymcr <d.barrowcliff@outlook.com>
2022-11-16 18:56:49 +0000

    p45

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Into The Mists | Joy Ingle</p>
+                  <p className="audio-player-text">P45 show w/ ekkinz</p>
                 </div>
               </ul>
             </div>
