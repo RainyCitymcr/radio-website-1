e85ee50d43c8720bb6de0668a66a819ba389d46c
rainy.city <d.barrowcliff@outlook.com>
2022-11-17 20:45:26 +0000

    on

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">relax and bake fm</p>
+                  <p className="audio-player-text">liil bits presents Apricity ft. jake Martin</p>
                 </div>
               </ul>
             </div>
