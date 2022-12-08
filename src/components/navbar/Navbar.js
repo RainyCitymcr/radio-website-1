cbc5d49664706a1b0c0b0e29acc4634f4d7c5793
rainy.city <d.barrowcliff@outlook.com>
2022-12-08 20:44:56 +0000

    aiku

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">flames disperse</p>
+                  <p className="audio-player-text">Aiku Kurotaki</p>
                 </div>
               </ul>
             </div>
