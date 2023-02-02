889b8e0d70e5a894724932ce65911e18334db874
Rainycitymcr <d.barrowcliff@outlook.com>
2023-02-02 18:03:47 +0000

    pciks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/navbar/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from "./components/home/Home";
 import Genres from './components/genres/Genres';
-import Picks from './components/cards/Cards';
+// import Picks from './components/cards/Cards';
 import Schedule from './components/schedule/Schedule';
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
           <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/picks" component={Picks} />
+            {/* <Route path="/picks" component={Picks} /> */}
             <Route path="/schedule" component={Schedule} />
             <Route path="/genres" component={Genres} />
             {/* <ComingSoon /> */}
