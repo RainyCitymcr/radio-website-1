feb56568f114ea9c44f7c54e53eb4d7ef95c0f52
rainy.city <d.barrowcliff@outlook.com>
2023-02-03 20:59:46 +0000

    alone

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">yuqq</p>
+                  <p className="audio-player-text">alone in the attic w/ dj bbygrinch</p>
                 </div>
               </ul>
             </div>
