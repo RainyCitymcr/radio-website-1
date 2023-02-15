f483e404241a583fad29c75548274d4f8b374d42
Rainycitymcr <d.barrowcliff@outlook.com>
2023-02-15 19:59:53 +0000

    linduk

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Jader Toya - Truck Cantine</p>
+                  <p className="audio-player-text">Lindrum</p>
                 </div>
               </ul>
             </div>
