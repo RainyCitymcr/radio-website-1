aadecf70e52d5efe50769776ec19238022cfd32d
Rainycitymcr <d.barrowcliff@outlook.com>
2023-02-15 18:52:09 +0000

    sood

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Disconnection with Paul East</p>
+                  <p className="audio-player-text">soood / blue pt. 2: manic</p>
                 </div>
               </ul>
             </div>
