d5a8500c746192f62ee42d309b5b5038ca66cf31
Rainycitymcr <d.barrowcliff@outlook.com>
2023-02-15 17:13:50 +0000

    paul

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">ADOMASLP</p>
+                  <p className="audio-player-text">Disconnection with Paul East</p>
                 </div>
               </ul>
             </div>
