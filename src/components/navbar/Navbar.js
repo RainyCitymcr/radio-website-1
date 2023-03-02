16d12bc8a7b471d3ec56dfd0e6c99b7b1f6a4cd8
Rainycitymcr <d.barrowcliff@outlook.com>
2023-03-02 18:27:47 +0000

    izzy

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">The Fat Britney</p>
+                  <p className="audio-player-text">izzy Iz</p>
                 </div>
               </ul>
             </div>
