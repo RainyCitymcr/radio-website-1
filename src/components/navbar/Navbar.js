d3f95fcf04d1441ed0c23b694644aab0ad26229b
digbybarrowcliff <d.barrowcliff@outlook.com>
2023-03-09 19:57:26 +0000

    bunney

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">alex.aubyn</p>
+                  <p className="audio-player-text">Bunney - Birthday Set!</p>
                 </div>
               </ul>
             </div>
