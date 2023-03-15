7270ee5727f1209ff49ea5ed3f06e328927f67a5
Rainycitymcr <d.barrowcliff@outlook.com>
2023-03-15 20:02:03 +0000

    lindru

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">p45 show w/ soza</p>
+                  <p className="audio-player-text">lindrum</p>
                 </div>
               </ul>
             </div>
