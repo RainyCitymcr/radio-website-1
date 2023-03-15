697070acd88df8325977c285da355e26a5c07adc
Rainycitymcr <d.barrowcliff@outlook.com>
2023-03-15 17:47:32 +0000

    wed on

--- src/components/schedule/Schedule.js
+++ src/components/schedule/Schedule.js
@@ -9,69 +9,69 @@ const Schedule = () => {
     return (
         <div className="schedule-container" id="schedule">
             <h1 className="schedule-title">SCHEDULE</h1>
-            {/* <table className="schedule-table">
+            <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Wednesday 16.11"
+                    showDate="Wednesday 15th March"
                 />
                     <ScheduleItem
                         showTime="18:00"
-                        showName= "Into the Mists - Joy Ingle"
+                        showName= "Rapatop Radio Tour w/ DJ Luh (Hollow Points Radio)"
                     />
                     <ScheduleItem
                         showTime="19:00"
-                        showName="P45 Show w/ Ekkinz"
+                        showName="P45 SHOW w/ SOZA"
                     />
                     <ScheduleItem
                         showTime="20:00"
-                        showName="Jerome Presents...Madness"
+                        showName="Lindrum"
                     />
-                    <ScheduleItem
+                    {/* <ScheduleItem
                         showTime="21:00"
                         showName= "Lindrum b2b aDeAd"
-                    />
-            </table> */}
+                    /> */}
+            </table>
             <table className="schedule-table">
                 <ScheduleDate 
-                    showDate="Thursday 10th March"
+                    showDate="Thursday 16th March"
                     />
                         <ScheduleItem
                             showTime="18:00"
-                            showName="DEAF LOOP"
-                        />
-                        <ScheduleItem
-                            showTime="19:00"
-                            showName="MC LOVE: DUB SESSION 1 "
+                            showName="MIRE"
                         />
                         <ScheduleItem
                             showTime="20:00"
-                            showName="BRUK It Down w/ Rendezvous Records"
+                            showName="Last Minute Film Club with Alex"
                         />
                         <ScheduleItem
                             showTime="21:00"
-                            showName="cabby & SFM: belated bonfire b2b"
+                            showName="Tazz"
+                        // />
+                        // <ScheduleItem
+                        //     showTime="21:00"
+                        //     showName="cabby & SFM: belated bonfire b2b"
                 />
             </table>
-            {/* <table className="schedule-table">
+            <table className="schedule-table">
                 <ScheduleDate 
                     showDate="Friday 3rd March"
                 />
                 <ScheduleItem
                     showTime="18:00"
-                    showName="Sarge"
+                    showName="CELSIUS"
                 />
                 <ScheduleItem
                     showTime="19:00"
-                    showName="Good and Proper w/ Wobbegong"
+                    showName="DJ Project Manager"
                 />
                 <ScheduleItem
                     showTime="20:00"
-                    showName="Yuqq"  
+                    showName="MBB"  
                 />
                 <ScheduleItem
                     showTime="21:00"
-                    showName="Rose A"
+                    showName="Unearth St. Patrick's Day Special / with ex.sess and Elsa"
                 />
-            </table> */}
+            </table>
         </div>
     )
 }
