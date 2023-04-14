232ab2b488ca3f582511dd09889de59f56150373
digbybarrowcliff <d.barrowcliff@outlook.com>
2023-04-14 18:49:33 +0100

    moregain

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">Jase Jeffery</p>
+                  <p className="audio-player-text">more gain</p>
                 </div>
               </ul>
             </div>
