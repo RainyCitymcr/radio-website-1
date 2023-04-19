3106d85547dcecfc594714e4ba282cae21d97840
digbybarrowcliff <d.barrowcliff@outlook.com>
2023-04-19 10:09:47 +0100

    on

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -113,11 +113,11 @@ const Navbar = () => {
               <ul>
                 <li><div className="circle red"></div></li>
                 {/* Write "ON" instead of "OFF" (or vice versa) */}
-                <li>off air</li>
+                <li>On air</li>
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text"></p>
+                  <p className="audio-player-text">Breakfast w/ Tommy, 4TGANG & Colou</p>
                 </div>
               </ul>
             </div>
