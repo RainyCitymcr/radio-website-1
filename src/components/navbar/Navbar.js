3c1808d6b89eed5b37141585c8de7102d2d81c9a
digbybarrowcliff <d.barrowcliff@outlook.com>
2023-04-21 19:59:47 +0100

    elsa

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -118,7 +118,7 @@ const Navbar = () => {
                 <div className="audio-player-container">
                   <Player />
                   {/* Inside the p tags here, write the DJ's name */}
-                  <p className="audio-player-text">DJ Project Manager</p>
+                  <p className="audio-player-text">unearth / elsa</p>
                 </div>
               </ul>
             </div>
